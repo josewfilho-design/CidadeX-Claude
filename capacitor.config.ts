a26bb8e2fd1bf8c6ad9ec8d8c86266293f3077d4
josewfilho-design <cidadexbr@gmail.com>
2026-03-08 19:09:01 -0300

    feat: alarme sonoro para remédios no horário agendado

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,10 @@ const config: CapacitorConfig = {
   appId: 'com.cidadex.br',
   appName: 'CidadeX',
   webDir: 'dist',
+  server: {
+    url: 'https://cidadex-br.com',
+    cleartext: false,
+  },
 };
 
 export default config;
